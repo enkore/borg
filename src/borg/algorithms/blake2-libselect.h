719a940c5a07b818381ebe6bd16da583fbffcd89
Marian Beermann <public@enkore.de>
2017-05-05 14:10:45 +0200

    blake2-libselect: explain purpose

--- src/borg/algorithms/blake2-libselect.h
+++ src/borg/algorithms/blake2-libselect.h
@@ -1,3 +1,8 @@
+/**
+ * Selects between system libb2 or vendored blake2 implementation.
+ * BORG_USE_LIBB2 is defined by the build driver (setup.py) if
+ * libb2 was detected and added to the include path.
+ */
 #ifdef BORG_USE_LIBB2
 #include <blake2.h>
 #else
